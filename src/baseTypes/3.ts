9a695988aae5873e9d324dbd3d3f701e71eb2271
Sergei-Bug <122543402+Sergei-Bug@users.noreply.github.com>
2023-11-12 10:23:55 +0200

    Task fixed

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,9 @@
 let some: unknown;
 some = "Text";
 let str: string;
-str = <string>some;
+
+if (typeof some === "string") {
+  str = some;
+}
 
 export {};
--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -14,6 +14,5 @@ enum DayOfWeek {
 }
 
 const isWeekend = (day: DayOfWeek): boolean => {
-  if (day === DayOfWeek.Saturday || day === DayOfWeek.Sunday) return true;
-  return false;
+  return day === DayOfWeek.Saturday || day === DayOfWeek.Sunday;
 };
